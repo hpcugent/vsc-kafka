f97bef21e9f99633551f27b44638f7db106f4224
Andy Georges <itkovian@gmail.com>
2023-02-07 14:39:16 +0100

    chore: headers

--- lib/vsc/__init__.py
+++ lib/vsc/__init__.py
@@ -2,14 +2,14 @@
 #
 # Copyright 2009-2023 Ghent University
 #
-# This file is part of vsc-filesystems,
+# This file is part of vsc-kafka,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
 # with support of Ghent University (http://ugent.be/hpc),
 # the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
 # the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
 # and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# https://github.com/hpcugent/vsc-filesystems
+# https://github.com/hpcugent/vsc-kafka
 #
 # All rights reserved.
 #
--- lib/vsc/kafka/__init__.py
+++ lib/vsc/kafka/__init__.py
@@ -10,18 +10,7 @@
 #
 # https://github.com/hpcugent/vsc-kafka
 #
-# vsc-kafka is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Library General Public License as
-# published by the Free Software Foundation, either version 2 of
-# the License, or (at your option) any later version.
-#
-# vsc-utils is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public License
-# along with vsc-utils. If not, see <http://www.gnu.org/licenses/>.
+# All rights reserved.
 #
 """
 Allow other packages to extend this namespace, zip safe setuptools style
