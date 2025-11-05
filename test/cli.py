7d96629b164975d2ad75ac4f1e970a847dc27238
Wouter Depypere <wouter.depypere@ugent.be>
2025-11-05 15:18:19 +0100

    fix mock replacement properly

--- test/cli.py
+++ test/cli.py
@@ -15,13 +15,13 @@
 """
 xdmod tests
 """
+
 import logging
-import unittest.mock
 import sys
 import json
 
+from unittest import mock
 from collections import namedtuple
-
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
@@ -31,14 +31,14 @@ from vsc.kafka.cli import make_time, ConsumerCLI
 
 from vsc.install.testing import TestCase
 
-class TestSlurm(TestCase):
 
+class TestSlurm(TestCase):
     def test_make_time(self):
-        ts = '1933142400'  # TZ=UTC date -d 2031-04-05T08:00:00 +%s
-        self.assertEqual(make_time(ts), '2031-04-05')
-        self.assertEqual(make_time(ts, fmt='%Y-%m-%dT%H:%M:%S'), '2031-04-05T08:00:00')
-        self.assertEqual(make_time(ts, fmt='%Y-%m-%dT%H:%M:%S', begin=True), '2031-04-05T00:00:00')
-        self.assertEqual(make_time(ts, fmt='%Y-%m-%dT%H:%M:%S', end=True), '2031-04-05T23:59:59')
+        ts = "1933142400"  # TZ=UTC date -d 2031-04-05T08:00:00 +%s
+        self.assertEqual(make_time(ts), "2031-04-05")
+        self.assertEqual(make_time(ts, fmt="%Y-%m-%dT%H:%M:%S"), "2031-04-05T08:00:00")
+        self.assertEqual(make_time(ts, fmt="%Y-%m-%dT%H:%M:%S", begin=True), "2031-04-05T00:00:00")
+        self.assertEqual(make_time(ts, fmt="%Y-%m-%dT%H:%M:%S", end=True), "2031-04-05T23:59:59")
 
 
 class TestConsumer(TestCase):
@@ -47,11 +47,13 @@ class TestConsumer(TestCase):
         super(TestConsumer, self).setUp()
 
         sys.argv = [
-            'name', '--debug',
-            '--brokers=serv1,serv2',
-            '--ssl=ssl1=sslv1,ssl2=sslv2',
-            '--sasl=sasl1=saslv1,sasl2=sslv2',
-            ]
+            "name",
+            "--debug",
+            "--brokers=serv1,serv2",
+            "--ssl=ssl1=sslv1,ssl2=sslv2",
+            "--sasl=sasl1=saslv1,sasl2=sslv2",
+        ]
+
     def mk_event(self, typ, resource, day, payload):
         return {
             "type": typ,
@@ -60,24 +62,22 @@ class TestConsumer(TestCase):
             "day": day,
         }
 
-
-    @mock.patch('vsc.utils.script_tools.ExtendedSimpleOption.prologue')
-    @mock.patch('vsc.kafka.cli.KafkaConsumer', autospec=True)
+    @mock.patch("vsc.utils.script_tools.ExtendedSimpleOption.prologue")
+    @mock.patch("vsc.kafka.cli.KafkaConsumer", autospec=True)
     def test_consume(self, mock_consumer, mock_prologue):
-
         cl1_1 = "test1_1|message1_1|cluster1|4|5|6|7|8|9|10|11|12|2020-02-28T01:02:03|14|15|16|17|18|19|20|21|22|23|24|25|26"
         cl1_2 = "test1_2|message1_1|cluster1|4|5|6|7|8|9|10|11|12|2020-02-28T11:02:03|14|15|16|17|18|19|20|21|22|23|24|25|26"
         cl2_1 = "test2_1|message2_1|cluster2|4|5|6|7|8|9|10|11|12|2020-02-29T04:05:06|14|15|16|17|18|19|20|21|22|23|24|25|26"
 
         def mk_msg(typ, resource, day, payload):
-            return KafkaMsg(value=json.dumps(self.mk_event(typ, resource, day, payload), sort_keys=True).encode('utf8'))
+            return KafkaMsg(value=json.dumps(self.mk_event(typ, resource, day, payload), sort_keys=True).encode("utf8"))
 
         KafkaMsg = namedtuple("KafkaMsg", ["value"])
 
         args = [
-            ('slurm', 'cluster1', '20200228', cl1_1),
-            ('slurm', 'cluster1', '20200228', cl1_2),
-            ('slurm', 'cluster2', '20200229', cl2_1),
+            ("slurm", "cluster1", "20200228", cl1_1),
+            ("slurm", "cluster1", "20200228", cl1_2),
+            ("slurm", "cluster2", "20200229", cl2_1),
         ]
 
         msgs = [mk_msg(*arg) for arg in args]
@@ -95,37 +95,40 @@ class TestConsumer(TestCase):
         # init of consumer
         name, args, kwargs = mock_consumer.mock_calls[0]
         logging.debug("%s %s %s", name, args, kwargs)
-        self.assertEqual(name, '')
-        self.assertEqual(args, ('xdmod',))  # topic
-        self.assertEqual(kwargs, {
-            'group_id': 'xdmod',
-            'security_protocol': 'PLAINTEXT',
-            'bootstrap_servers': ['serv1', 'serv2'],
-            'ssl1': 'sslv1',
-            'ssl2': 'sslv2',
-            'sasl1': 'saslv1',
-            'sasl2': 'sslv2',
-            'enable_auto_commit': True,
-        })
+        self.assertEqual(name, "")
+        self.assertEqual(args, ("xdmod",))  # topic
+        self.assertEqual(
+            kwargs,
+            {
+                "group_id": "xdmod",
+                "security_protocol": "PLAINTEXT",
+                "bootstrap_servers": ["serv1", "serv2"],
+                "ssl1": "sslv1",
+                "ssl2": "sslv2",
+                "sasl1": "saslv1",
+                "sasl2": "sslv2",
+                "enable_auto_commit": True,
+            },
+        )
 
         # next call is the __iter__ call for the loop
         name, args, kwargs = mock_consumer.mock_calls[1]
         logging.debug("%s %s %s", name, args, kwargs)
-        self.assertEqual(name, '().__iter__')
+        self.assertEqual(name, "().__iter__")
         self.assertEqual(args, ())
         self.assertEqual(kwargs, {})
 
         # next are the message commits
         for idx in range(len(msgs)):
-            name, args, kwargs = mock_consumer.mock_calls[2+idx]
+            name, args, kwargs = mock_consumer.mock_calls[2 + idx]
             logging.debug("%s %s %s", name, args, kwargs)
-            self.assertEqual(name, '().commit')
+            self.assertEqual(name, "().commit")
             self.assertEqual(args, ())
             self.assertEqual(kwargs, {})
 
         # last call is the close call
         name, args, kwargs = mock_consumer.mock_calls[-1]
         logging.debug("%s %s %s", name, args, kwargs)
-        self.assertEqual(name, '().close')
+        self.assertEqual(name, "().close")
         self.assertEqual(args, ())
-        self.assertEqual(kwargs, {'autocommit': True})
+        self.assertEqual(kwargs, {"autocommit": True})
